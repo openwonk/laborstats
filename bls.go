3c3f63d480804f847cdb98f968d806bb8c1ad29e
Bryan Davis <openwonk@users.noreply.github.com>
2015-01-02 15:07:18 -0800

    Update bls.go

--- bls.go
+++ bls.go
@@ -1,7 +1,7 @@
 package bls
 
 import (
-        // "bytes"
+        "bytes"
         "fmt"
         "io/ioutil"
         "net/http"
@@ -10,7 +10,6 @@ import (
 // example series "LAUCN040010000000005"
 func SingleSeries(series string) string {
         url := "http://api.bls.gov/publicAPI/v2/timeseries/data/"
-        // var jsonStr = []byte(`{"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2011", "endyear":"2014"}`)
         req, err := http.NewRequest("POST", url+series, nil)
         req.Header.Set("Content-Type", "application/json")
 
@@ -26,6 +25,26 @@ func SingleSeries(series string) string {
         
         return string(body)
 }
+
+func MutipleSeries(series ...string) {
+        series := `{"seriesid":["LAUCN040010000000005", "LAUCN040010000000006"]}`
+        url := "http://api.bls.gov/publicAPI/v2/timeseries/data/"
+
+        jsonStr := []byte(series)
+        req, err := http.NewRequest("POST", url, bytes.NewReader(jsonStr))
+        req.Header.Set("Content-Type", "application/json")
+
+        client := &http.Client{}
+        resp, err := client.Do(req)
+        check(err)
+        defer resp.Body.Close()
+
+        fmt.Println("response Status:", resp.Status)
+        fmt.Println("response Headers:", resp.Header)
+        body, _ := ioutil.ReadAll(resp.Body)
+        fmt.Println("response Body:", string(body))
+}
+
 
 func check (err error) {
         if err != nil {
