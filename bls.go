e17e8e71965be23e3bf18bd494b97ee57425440b
Bryan Davis <openwonk@users.noreply.github.com>
2015-01-02 13:52:42 -0800

    Create bls.go

---
+++ bls.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+        // "bytes"
+        "fmt"
+        "io/ioutil"
+        "net/http"
+)
+
+func main() {
+        series := "LAUCN040010000000005"
+        url := "http://api.bls.gov/publicAPI/v2/timeseries/data/"
+        fmt.Println("URL:>", url)
+
+        // var jsonStr = []byte(`{"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2011", "endyear":"2014"}`)
+        req, err := http.NewRequest("POST", url+series, nil)
+        req.Header.Set("Content-Type", "application/json")
+
+        client := &http.Client{}
+        resp, err := client.Do(req)
+        if err != nil {
+                panic(err)
+        }
+        defer resp.Body.Close()
+
+        fmt.Println("response Status:", resp.Status)
+        fmt.Println("response Headers:", resp.Header)
+        body, _ := ioutil.ReadAll(resp.Body)
+        fmt.Println("response Body:", string(body))
+}
