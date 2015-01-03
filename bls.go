fb452ac8e4ca2ea3035b3a587829d7ad1683601c
openwonk <bryan@neoty.pe>
2015-01-03 08:36:31 -0800

    auto commit

--- bls.go
+++ bls.go
@@ -2,34 +2,23 @@ package bls
 
 import (
 	"bytes"
-	// "fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
 )
 
-type Request struct {
+const (
+	url = "http://api.bls.gov/publicAPI/v2/timeseries/data/"
+)
+
+type Series struct {
 	RegistrationKey, StartYear, EndYear  string
 	Catalog, Calculations, AnnualAverage bool
 	Series                               []string
 }
 
-func (r *Request) SingleSeries() string {
-	url := "http://api.bls.gov/publicAPI/v2/timeseries/data/"
-	req, err := http.NewRequest("POST", url+r.Series[0], nil)
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	check(err)
-	defer resp.Body.Close()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-	return string(body)
-}
-
-func (r *Request) MultipleSeries() string {
+func (r *Series) Request() string {
 	// "LAUCN040010000000005", "LAUCN040010000000006"
 	payload := `{
 		"seriesid":[` + "\"" + strings.Join(r.Series, "\",\"") + "\"" + `],
@@ -38,8 +27,6 @@ func (r *Request) MultipleSeries() string {
 		"calculations":` + strconv.FormatBool(r.Calculations) + `,
 		"annualaverage":` + strconv.FormatBool(r.AnnualAverage) + `,
 		"registrationKey":"` + r.RegistrationKey + `"}`
-
-	url := "http://api.bls.gov/publicAPI/v2/timeseries/data/"
 
 	jsonStr := []byte(payload)
 	req, err := http.NewRequest("POST", url, bytes.NewReader(jsonStr))
